a0f28407d7a5682c66e1181a02685ca5cf01c205
Liuyll <540374501@qq.com>
2026-03-02 21:52:52 +0800

    Initial Commit

---
+++ WeItems/WeItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeItemsApp.swift
+//  WeItems
+//
+//  Created by yl Liu on 2026/3/2.
+//
+
+import SwiftUI
+
+@main
+struct WeItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
